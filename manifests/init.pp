6c4b54c441cd8f20ca10c974c622a99dc8d7a85b
unknown <j00273701@S00304949P61726.china.huawei.com>
2015-12-21 19:40:29 +0800

    add puppet onos scripts for apex

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+class onos($controllers_ip){
+
+$ovs_manager_ip = $controllers_ip[0]
+
+$onos_home = '/opt/onos'
+$onos_pkg_url = 'http://downloads.onosproject.org/release/onos-1.3.0.tar.gz'
+$karaf_dist = 'apache-karaf-3.0.3'
+$onos_pkg_name = 'onos-1.3.0.tar.gz'
+$jdk8_pkg_name = 'jdk-8u51-linux-x64.tar.gz'
+$onos_boot_features = 'config,standard,region,package,kar,ssh,management,webconsole,onos-api,onos-core,onos-incubator,onos-cli,onos-rest,onos-gui,onos-openflow-base,onos-openflow'
+$onos_extra_features = 'ovsdb,vtn'
+
+class {'::onos::install':}->
+class {'::onos::config':}->
+class {'::onos::service':}->
+class {'::onos::ovs':
+      manager_ip => $ovs_manager_ip}
+}
---
+++ manifests/service.pp
@@ -0,0 +1,39 @@
+class onos::service{
+
+Exec{
+        path => "/usr/bin:/usr/sbin:/bin:/sbin",
+        timeout => 360,
+        logoutput => 'true',
+}
+firewall {'215 onos':
+      port   => [ 6633, 6640, 6653, 8181, 8101,9876],
+      proto  => 'tcp',
+      action => 'accept',
+}->
+exec{ 'start onos':
+        command => 'service onos start',
+        unless => 'service onos status | grep PID'
+}->
+
+#service{ 'onos':
+#        ensure => running,
+#        enable => true,
+#}->
+exec{ 'sleep 100 to stablize onos':
+        command => 'sudo sleep 100;'
+}->
+exec{ 'install openflow feature':
+        command => "/opt/onos/bin/onos 'feature:install onos-openflow'"
+}->
+exec{ 'install openflow-base feature':
+        command => "/opt/onos/bin/onos 'feature:install onos-openflow-base'"
+}->
+exec{ 'add onos auto start':
+        command => 'sudo echo "onos">>/opt/service',
+        logoutput => "true",
+}->      
+exec{ 'set public port':
+        command => "/opt/onos/bin/onos \"externalportname-set -n ens4\""
+}      
+
+}
